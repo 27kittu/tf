933bc5fd6151d2346157c308565f2b9355854f61
27kittu <cv27.one@gmail.com>
2025-02-11 23:05:24 +0530

    modified files

--- practise/aws/practise_1/main.tf
+++ practise/aws/practise_1/main.tf
@@ -2,11 +2,8 @@ resource "aws_s3_bucket" "b1" {
   bucket = "b1-new1"
 }
 
-data "aws_ami" "ubuntu" {
-  most_recent = true
-
 resource "aws_instance" "web" {
-  ami           = data.aws_ami.ubuntu.id
+  ami           = "i-k24k35k45k35" 
   instance_type = "t3.micro"
 
   tags = {
