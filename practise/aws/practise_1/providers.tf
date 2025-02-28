8b35046e84904aa6b64a21bfb8c7cc86d682a12d
27kittu <cv27.one@gmail.com>
2025-03-01 01:42:48 +0530

    few files modified

--- practise/aws/practise_1/providers.tf
+++ practise/aws/practise_1/providers.tf
@@ -4,7 +4,7 @@ provider "aws" {
   region                      = "us-east-1"
   skip_credentials_validation = true
   skip_metadata_api_check     = true
-  s3_force_path_style         = true
+  #s3_force_path_style         = true
   endpoints {
     sts      = "http://localhost:4566"
     s3       = "http://localhost:4566"
