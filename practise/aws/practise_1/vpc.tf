7bba2b193c3d1536facf3583dda0a97b6991477b
27kittu <cv27.one@gmail.com>
2025-04-07 12:52:00 +0000

    updated few files

--- practise/aws/practise_1/vpc.tf
+++ practise/aws/practise_1/vpc.tf
@@ -17,7 +17,7 @@ resource "aws_subnet" "subnet1" {
 }
 
 resource "aws_route_table" "rt1" {
-  vpc_id   = aws_vpc.vpc1.id
+  vpc_id = aws_vpc.vpc1.id
   route {
     cidr_block = "192.168.0.0/26"
     gateway_id = aws_internet_gateway.gw1.id
